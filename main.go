c286545539e0bc9b72be92d77dbb16a48e5899e1
Luiz Filho <luizbafilho@gmail.com>
2018-07-25 21:08:03 -0300

    Implementing UrlChecker.Check

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net"
 	"net/url"
 	"regexp"
@@ -23,20 +24,38 @@ type Checker interface {
 }
 
 type UrlChecker struct {
+	Timeout time.Duration
 }
 
 // very naive regex I got on google, it won't cover all cases, but the basic ones should
 var reURL = regexp.MustCompile(`(?im)https?:\/\/[A-Z\d\.-]{2,}\.[A-Z]{2,}(:\d{2,4})?`)
 
-// Check checks for reachable urls in the Pull Request body
-func (uc *UrlChecker) Check(pr *PullRequest) ([]URLStatus, error) {
-	statuses := []URLStatus{}
+// Check parses the pr's body looking for urls and checks their reachability
+func (uc *UrlChecker) Check(pr *PullRequest) []URLStatus {
+	// extracting all urls from pr's body
+	matches := reURL.FindAllString(pr.Body, -1)
+	matchesLen := len(matches)
 
-	return statuses, nil
+	statusesCh := make(chan URLStatus, matchesLen)
+	for _, m := range matches {
+		go func(url string) {
+			status, err := uc.urlReachable(url)
+			if err != nil {
+				log.Printf("url=%s is not reachable: %s", url, err)
+			}
+			statusesCh <- URLStatus{url, status}
+		}(m)
+	}
+
+	statuses := []URLStatus{}
+	for i := 0; i < matchesLen; i++ {
+		statuses = append(statuses, <-statusesCh)
+	}
+
+	return statuses
 }
 
-func urlReachable(u string) (bool, error) {
-	timeout := 10 * time.Second
+func (uc *UrlChecker) urlReachable(u string) (bool, error) {
 	parsedURL, err := url.Parse(u)
 	if err != nil {
 		return false, err
@@ -52,7 +71,7 @@ func urlReachable(u string) (bool, error) {
 		}
 	}
 
-	conn, err := net.DialTimeout("tcp", address, timeout)
+	conn, err := net.DialTimeout("tcp", address, uc.Timeout)
 	if err != nil {
 		return false, err
 	}
--- main_test.go
+++ main_test.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCheck(t *testing.T) {
+	pr := &PullRequest{
+		Body: `Lorem ipsum dolor sit ame http://www.google.com
+		llo inventore veritatis https://yahoo.com:443 et quasi
+		architecto beatae vitae dicta sunt explicabo.  consequuntur magni
+		dolores eos qui http://foo.local:8888
+		`,
+	}
+
+	uc := UrlChecker{
+		Timeout: 1 * time.Second,
+	}
+	assert.Equal(t, []URLStatus{
+		{"http://www.google.com", true},
+		{"https://yahoo.com:443", true},
+		{"http://foo.local:8888", false},
+	}, uc.Check(pr))
+}
+
 func TestUrlReachable(t *testing.T) {
+	uc := UrlChecker{
+		Timeout: 1 * time.Second,
+	}
 	var cases = []struct {
 		url     string
 		status  bool
@@ -21,7 +44,7 @@ func TestUrlReachable(t *testing.T) {
 	for _, c := range cases {
 		t.Run(c.url, func(t *testing.T) {
 			t.Parallel()
-			reachable, err := urlReachable(c.url)
+			reachable, err := uc.urlReachable(c.url)
 			assert.Equal(t, c.noError, err == nil)
 			assert.Equal(t, c.status, reachable)
 		})
